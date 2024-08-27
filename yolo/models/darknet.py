e0add3770156a09b159ca08c58abb4f5f99bb003
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-27 12:06:25 +0100

    create a method to create a YOLO convolutional block

--- yolo/models/darknet.py
+++ yolo/models/darknet.py
@@ -1,11 +1,13 @@
 import torch
 from torch import nn
+from typing import List
 
 class DarkNet(nn.Module):
     def __init__(self, in_channels: int = 3):
         self.__init__()
         self.in_channels = in_channels
-        self.conv1 = nn.Conv2d(in_channels, 64, 7, stride=2, padding=1)
+        self.block1 = self._make_conv_bn_layer(in_channels, 64, 7, 2)
+        self.block2 = self._make_conv_bn_layer(64, 192, 3)
         # TODO
         raise NotImplementedError("DarkNet constructor not implemented")
 
@@ -14,8 +16,8 @@ class DarkNet(nn.Module):
         raise NotImplementedError("DarkNet forward not implemented")
 
     def _make_conv_bn_layer(in_channels: int, out_channels: int, kernel_size: int, stride: int = 1, padding: int = 0, with_pool: bool = True):
-        layers = [
-                nn.Conv2d(in_channels, out_channnels, kernel_size, stride, padding),
+        layers: List[nn.Module] = [
+                nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
                 nn.BatchNorm2d(out_channels),
                 nn.LeakyReLU(0.1, inplace=True)
         ]
--- yolo/models/yolov1.py
+++ yolo/models/yolov1.py
@@ -1,12 +1,21 @@
 import torch
 from torch import nn
+from darknet import DarkNet
 
 # TODO
 
 class YOLOv1(nn.Module):
-    def __init__(self):
+    """
+    YOLOv1 object detection neural network.
+    First version of the family. 
+    """
+    def __init__(self, in_channels: int = 3) -> None:
+        # TODO
+        self.in_channels = in_channels
+        self.backbone = DarkNet(in_channels)
         raise NotImplementedError("YOLOv1 constructor not implemented")
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # TODO
         raise NotImplementedError("YOLOv1 forward not implemented")
 
