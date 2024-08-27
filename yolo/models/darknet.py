110ccef3bd8dd740bb76d3cbee4d635ea109303e
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-27 16:25:21 +0100

    implement the darknet backbone

--- yolo/models/darknet.py
+++ yolo/models/darknet.py
@@ -1,28 +1,29 @@
 import torch
 from torch import nn
-from typing import List
+from typing import List, Any
+from utils.yolo import make_conv_bn_layer
 
 class DarkNet(nn.Module):
-    def __init__(self, in_channels: int = 3):
+    def __init__(self, config: Any):
         self.__init__()
-        self.in_channels = in_channels
-        self.block1 = self._make_conv_bn_layer(in_channels, 64, 7, 2)
-        self.block2 = self._make_conv_bn_layer(64, 192, 3)
-        # TODO
-        raise NotImplementedError("DarkNet constructor not implemented")
+        self.in_channels = config.in_channels
+        self.conv_blocks = self.generate_conv_blocks(config)
 
     def forward(self, x) -> torch.Tensor:
-        # TODO
-        raise NotImplementedError("DarkNet forward not implemented")
+        x = self.conv_blocks(x)
+        return x
+    
+    def generate_conv_blocks(self, config: Any) -> nn.Sequential:
 
-    def _make_conv_bn_layer(in_channels: int, out_channels: int, kernel_size: int, stride: int = 1, padding: int = 0, with_pool: bool = True):
-        layers: List[nn.Module] = [
-                nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
-                nn.BatchNorm2d(out_channels),
-                nn.LeakyReLU(0.1, inplace=True)
-        ]
-        if with_pool:
-            layers.append(nn.MaxPool2d(2, 2))
+        in_channels = self.in_channels
 
-        return nn.Sequential(*layers)
+        blocks: List[nn.Module] = []
 
+        # iterate the conv blocks description
+        for block in config.conv_blocks:
+            # iterate the block layers
+            for layer in block.layers:
+                blocks.append(make_conv_bn_layer(in_channels, layer[0], layer[1], layer[2], layer[3], True))
+                in_channels = layer[0]
+
+        return nn.Sequential(*blocks)
