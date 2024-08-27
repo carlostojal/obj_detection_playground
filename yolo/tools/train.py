0675f448deb2c2d812b77b451c78a50ac8946b43
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-27 16:34:18 +0100

    build the model on the training script

--- yolo/tools/train.py
+++ yolo/tools/train.py
@@ -3,15 +3,22 @@ from argparse import ArgumentParser
 import os
 import sys
 import yaml
+sys.path.append(".")
+from models.yolov1 import YOLOv1
 
 if __name__ == "__main__":
-    print("YOLO TRAINING SCRIPT")
+
+    print("** YOLOv1 Training Script **\n")
+
     # parse command line arguments
+    print("Parsing command line arguments...", end=" ")
     parser = ArgumentParser()
     parser.add_argument("--conf", "-c", help="Path to YOLO configuration file", type=str, required=True)
     args = parser.parse_args()
+    print("Done.")
 
     # parse the YOLO configuration file
+    print("Parsing the YOLO configuration file...", end=" ")
     if not os.path.isfile(args.conf):
         print("Provided conf. file path is not a file")
         sys.exit(-1)
@@ -22,13 +29,24 @@ if __name__ == "__main__":
     try:
         h = open(args.conf, "r")
     except Exception as e:
-        print("Error opening file: ", e)
+        print("Error opening configuration file: ", e)
         sys.exit(-3)
     # parse the YAML configuration
     conf = yaml.safe_load(h)
+    try:
+        h.close()
+    except Exception as e:
+        print("Error closing configuration file: ", e)
+        sys.exit(-4)
+    print("Done.")
 
-    print(f"YOLO configuration: {conf}")
+    print(conf)
 
-    raise NotImplementedError("YOLO training script not implemented")
+    # build the model
+    print("Building the model...", end=" ")
+    model = YOLOv1(conf)
+    print("Done.")
+
+    print(model)
 
     sys.exit(0)
