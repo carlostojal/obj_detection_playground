39471efab126053088b9a05178d3c7b2912cf681
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-08-31 23:51:45 +0100

    load the samples from the fiftyone dataset

--- datasets/FSOCO_FiftyOne.py
+++ datasets/FSOCO_FiftyOne.py
@@ -2,14 +2,25 @@ import torch
 from torch.utils.data import Dataset
 from PIL import Image
 import fiftyone as fo
+from torchvision.transforms import transforms
 from typing import Tuple
+
+classes_dict = {
+    "yellow_cone": 0,
+    "blue_cone": 1,
+    "orange_cone": 2,
+    "large_orange_cone": 3,
+    "unknown_cone": 4
+}
 
 class FSOCO_FiftyOne(Dataset):
     """
     FSOCO dataset loaded from FiftyOne.
     """
 
-    def __init__(self, split: str = "train", fiftyone_name: str = "fsoco") -> None:
+    def __init__(self, split: str = "train", fiftyone_name: str = "fsoco",
+                 img_width: int = 640, img_height: int = 480,
+                 max_boxes: int = 30) -> None:
         """
         Initialize an instance of the FSOCO dataset.
 
@@ -23,12 +34,18 @@ class FSOCO_FiftyOne(Dataset):
 
         self.split = split
         self.fiftyone_name = fiftyone_name
+        self.img_width = img_width
+        self.img_height = img_height
+        self.max_boxes = max_boxes
 
         # load the FiftyOne dataset
         self.dataset = fo.load_dataset(self.fiftyone_name)
 
         # get a view of the samples of the split
         self.samples = self.dataset.match_tags(self.split)
+
+        # get the sample IDs
+        self.sample_ids = self.samples.values("id")
 
     def __len__(self) -> int:
         """
@@ -48,8 +65,29 @@ class FSOCO_FiftyOne(Dataset):
             idx (int): Index of the sample.
 
         Returns:
-            (torch.Tensor): Image tensor shaped (batch_size, num_channels, height, width).
-            (torch.Tensor): Bounding boxes tensor shaped (batch_size, n_boxes, 5) where the last dimension is [x, y, h, w, class].
+            (torch.Tensor): Image tensor shaped (num_channels, height, width).
+            (torch.Tensor): Bounding boxes tensor shaped (max_boxes, 5) where the last dimension is [x, y, h, w, class].
         """
 
-        # TODO
+        # get the sample ID
+        sample = self.samples[self.sample_ids[idx]]
+
+        # get the image
+        img = Image.open(sample.filepath)
+        img = img.convert("RGB")
+        img = img.resize((self.img_width, self.img_height))
+        
+        # convert the image to tensor
+        transform = transforms.ToTensor()
+        img = transform(img)
+
+        # create a tensor of zeros for the bounding boxes
+        bboxes = torch.zeros((self.max_boxes, 5))
+
+        # iterate the ground truth objects
+        cur_box: int = 0
+        for detection in sample['ground_truth']['detections']:
+            bboxes[cur_box, :4] = torch.tensor(detection.bounding_box)
+            bboxes[cur_box, 4] = classes_dict[detection.label]
+
+        return img, bboxes
