75de9a54b57fa7c401bcd73740a8fe3ee3c72580
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-09-03 11:23:55 +0100

    implement unit testing in the padding utilities

---
+++ tests/test.py
@@ -0,0 +1,5 @@
+import unittest
+from utils.padding import TestPadding
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/utils/padding.py
+++ tests/utils/padding.py
@@ -1,5 +1,7 @@
 import unittest
 import torch
+import sys
+sys.path.append('.')
 from datasets.utils import pad_image, unpad_bboxes
 
 class TestPadding(unittest.TestCase):
@@ -10,18 +12,15 @@ class TestPadding(unittest.TestCase):
         target_size = (480, 640)
         padded_img, padded_bboxes, padding = pad_image(img, bboxes, target_size)
         self.assertEqual(padded_img.size(), (3, 480, 640))
-        self.assertEqual(padding, (520, 1360))
+        self.assertEqual(padding, (160, 0))
 
     def test_pad_bboxes(self):
         img = torch.rand(3, 1000, 2000)
-        bboxes = torch.tensor([0.1, 0.1, 0.1, 0.1, 0], [0.2, 0.2, 0.2, 0.2, 1])
+        bboxes = torch.tensor([[0.1, 0.1, 0.1, 0.1, 0], [0.2, 0.2, 0.2, 0.2, 1]])
         target_size = (480, 640)
         padded_img, padded_bboxes, padding = pad_image(img, bboxes, target_size)
         self.assertEqual(padded_bboxes.size(), (2, 5))
         self.assertEqual(padded_bboxes[0, 0], 0.1)
-        self.assertAlmostEqual(padded_bboxes[0, 1], 2/30)
+        self.assertAlmostEqual(padded_bboxes[0, 1], 7/30)
         self.assertAlmostEqual(padded_bboxes[0, 2], 2/30)
         self.assertAlmostEqual(padded_bboxes[0, 3], 0.1)
-
-
-
